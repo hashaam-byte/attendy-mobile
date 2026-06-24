779772a301c9b7549897dc39166b9240365ed5c8
hashaam <hashaamustafa@gmail.com>
2026-06-24 21:50:49 +0100

    ...

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,6 @@
-// babel.config.js
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ['module-resolver', { root: ['./src'], alias: { '@': './src' } }]
-    ],
   };
 };
